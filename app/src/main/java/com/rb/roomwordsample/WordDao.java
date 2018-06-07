8798f3984322da67224d42e6f4a667fef7427f4f
Robert <robbooda@gmail.com>
2018-06-07 20:06:56 +0200

    Create the DAO

---
+++ app/src/main/java/com/rb/roomwordsample/WordDao.java
@@ -0,0 +1,21 @@
+package com.rb.roomwordsample;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    @Insert
+    void insert (Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    List<Word> getAllWords();
+}
